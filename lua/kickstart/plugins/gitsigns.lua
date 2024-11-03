1a218e16e62b04a5ac6afeb524eb49b36e3cddae
rob <robiniscoding@gmail.com>
2024-11-03 11:13:17 +0100

    update

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -7,6 +7,10 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 0,
+        virt_text_pos = 'right_align',
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
