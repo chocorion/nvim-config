4cdc61f4e10ff6e9968027a2a52705fa6741958f
rob <robiniscoding@gmail.com>
2024-11-03 12:26:26 +0100

    use forked theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
--- return {
---   'zenbones-theme/zenbones.nvim',
---   dependencies = 'rktjmp/lush.nvim',
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.o.background = 'light'
---     vim.cmd.colorscheme 'zenbones'
---   end,
--- }
-
 return {
-  'shatur/neovim-ayu',
+  'chocorion/neovim-ayu',
   lazy = false,
   priority = 1000,
   config = function()
