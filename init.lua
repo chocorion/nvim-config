b4caf7f120a9861efe6670128e8362e94511b3ef
rob <robiniscoding@gmail.com>
2024-12-30 16:25:36 +0100

    remove unused plugin

--- init.lua
+++ init.lua
@@ -166,8 +166,6 @@ require('lazy').setup {
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.comment').setup()
-      -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
